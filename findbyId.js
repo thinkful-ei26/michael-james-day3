88f26e52dc7f1e6890fac6c8a3f657ba14cdb978
James Bell <Noxachron@gmail.com>
2018-10-31 16:26:28 -0400

    added Find by Id drill

---
+++ findbyId.js
@@ -0,0 +1,20 @@
+'use strict';
+/*findById. This function takes 2 arguments, items and idNum.
+
+items is an array of objects.
+ idNum is the id we're trying to find in items.
+  The function should look for an item with the id idNum in the array items.
+   If found, it should return that item.*/
+
+const data = [{id: 1, foo: 'bar'}, {id: 2, foo: 'bizz'}]
+
+function findById(items, idNum){
+    const found = items.find(element => {
+        if (element.id === idNum){
+            return element;
+        }
+    });
+    return found;
+}
+
+console.log(findById(data, 2));
