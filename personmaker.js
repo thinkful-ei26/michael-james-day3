e7d53e1dcf6c5198e1aac54630cb7a7961c8cc5d
James Bell <Noxachron@gmail.com>
2018-10-31 14:52:29 -0400

    more this and this

--- personmaker.js
+++ personmaker.js
@@ -1,3 +1,4 @@
+'use strict';
 // Modify personMaker.fullName to be a function that uses self-reference (via this) in order to return the firstName and lastName properties separated by a space.
 
 // So, for instance, if firstName was 'Jane' and lastName was 'Doe', fullName() should return 'Jane Doe'.
@@ -7,11 +8,19 @@ function personMaker() {
     var person = {
       firstName: 'Paul',
       lastName: 'Jones',
+      age: 10,
+      
       // replace `null` with a function that uses self reference to return
       // full name
       fullName: function () {
         return `${this.firstName} ${this.lastName}`;  
       } ,
+      stats: {
+       hair: 'brown',
+        species: function(){ return this;}
+      }
+
+
     };
     return person;
   }
@@ -22,3 +31,4 @@ function personMaker() {
   //console.log(michael.fullName);
   console.log(michael.fullName());
   //console.log(michael);
+  console.log(michael.stats.species.call(michael));
