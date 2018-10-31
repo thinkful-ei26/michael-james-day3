d59e71bad9396b7ec0a1b64e85a713a6b827d374
James Bell <Noxachron@gmail.com>
2018-10-31 17:34:12 -0400

    Hobbits 4th meal drill

---
+++ arraysInObjects.js
@@ -0,0 +1,6 @@
+'use strict';
+const myObj ={
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(`Hobbits 4th meal is: ${myObj.meals[3]}`);
