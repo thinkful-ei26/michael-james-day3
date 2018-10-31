0ad994c966356caecdb49ffdc4c0478d2a4904d0
James Bell <Noxachron@gmail.com>
2018-10-31 12:15:20 -0400

    added updated func

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+'use strict';
 // In this drill, you need to write code for the createMyObject function so that it returns an object with the following key value pairs:
 
 // foo => 'bar'
@@ -10,6 +11,30 @@ function objectCreator() {
     return { foo: 'bar', answerToUniverse: 42, 'olly olly': 'oxen free', sayHello: () => 'hello' };
 }
 
-console.log(objectCreator());
+function updateObject(obj){
+    return (obj.foo = 'foo', obj.bar = 'bar', obj.bizz = 'bizz', obj.bang = 'bang');
+}
 
-//const obj = Object.assign(objectCreator());
+
+
+const myTestObj = Object.assign(objectCreator());
+const myTestObj2 = objectCreator();
+
+console.log(`myfirst object is set to: ${(myTestObj.foo)}`);
+console.log(`mysecond object is set to: ${Object.keys(myTestObj2)}`);
+
+updateObject(myTestObj);
+updateObject(myTestObj2);
+
+console.log(`object is updated to: ${myTestObj.foo}`);
+console.log(`mysecond object is updated to: ${Object.keys(myTestObj2)}`);
+
+
+
+// Modify the updateObject function (which takes a single argument (obj)) so that it adds the following key/value pairs to obj and returns it:
+
+// foo => 'foo'
+// bar => 'bar'
+// bizz => 'bizz'
+// bang => 'bang'
+
