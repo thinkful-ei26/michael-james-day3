423cfd6409496b15973aec00691e9241cc4d2417
Michael McKay <mamckay89@gmail.com>
2018-10-31 15:40:15 -0600

    added objArrays.js

---
+++ objArrays.js
@@ -0,0 +1,7 @@
+
+
+const obj = [{name: 'henry', jobTitle: 'cars'},{name: 'mike', jobTitle: 'janitor'},{name: 'james', jobTitle: 'programmer'}]   
+
+obj.forEach(element => {
+    console.log(element.name, element.jobTitle)    
+});
